cc9785c76ea0bb063e217ee119388a98e152fe3a
Lê Ngọc Thắng <lethangng@gmail.com>
2023-02-23 15:32:00 +0700

    Giai quyet xung dot

--- routes/web.php
+++ routes/web.php
@@ -43,21 +43,3 @@ Route::get('/quanlynhanvien',function (){
  return view('nhansu/quanlynhanvien');
 })->name('quanlynhanvien');
 
-Route::prefix('khenthuong')->group(function() {
-    Route::get('/', [KhenThuongController::class, 'index'])->name('khenthuong.index');
-    Route::get('create', [KhenThuongController::class, 'create'])->name('khenthuong.create');
-    Route::post('create', [KhenThuongController::class, 'store']);
-    Route::get('{id}/edit', [KhenThuongController::class, 'edit'])->whereNumber('id')->name('khenthuong.edit');
-    Route::put('{id}/edit', [KhenThuongController::class, 'update'])->whereNumber('id')->name('khenthuong.update');
-    Route::delete('{id}/destroy', [KhenThuongController::class, 'destroy'])->whereNumber('id')->name('khenthuong.destroy');
-});
-
-Route::prefix('khenthuong-canhan')->group(function() {
-    Route::get('/', [KhenThuong_CaNhanController::class, 'index'])->name('khenthuong_canhan.index');
-    Route::get('create', [KhenThuong_CaNhanController::class, 'create'])->name('khenthuong_canhan.create');
-    Route::post('create', [KhenThuong_CaNhanController::class, 'store']);
-    Route::get('{id}/edit', [KhenThuong_CaNhanController::class, 'edit'])->whereNumber('id')->name('khenthuong_canhan.edit');
-    Route::put('{id}/edit', [KhenThuong_CaNhanController::class, 'update'])->whereNumber('id')->name('khenthuong_canhan.update');
-    Route::delete('{id}/destroy', [KhenThuong_CaNhanController::class, 'destroy'])->whereNumber('id')->name('khenthuong_canhan.destroy');
-    Route::post('search', [KhenThuong_CaNhanController::class, 'search'])->name('khenthuong_canhan.search');
-});
