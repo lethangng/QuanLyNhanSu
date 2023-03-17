ca396a4fc74d97f087590f012bec49d8879c9bd0
Pham-Khanh-Long <phamkhanhlong.nb35@gmail.com>
2023-03-18 00:36:39 +0700

    css ql thong ke

--- resources/views/thongke/qlbaocaothongke.blade.php
+++ resources/views/thongke/qlbaocaothongke.blade.php
@@ -27,20 +27,10 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-4 mid">
-                        <a href="">
-                            <div class="container-icon-thongke">
-                                <span class="icon-thongke">
-                                    <img src="{{ asset('icon/icon-thongke-1.png') }}" alt="">
-                                </span>
-                                <h4>Thống kê kỷ luật</h4>
-                            </div>
-                        </a>
-                    </div>
                 </div>
 
                 <div class="row bot">
-                    {{-- <div class="col left">
+                    <div class="col left">
                         <form action="{{ route('thongketangluong') }}" method="post">
                             @php
                                 use Carbon\Carbon;
@@ -55,16 +45,6 @@
                                 <h4>Thống kê tăng lương</h4>
                             </button>
                         </form>
-                    </div> --}}
-                    <div class="col-4 mid">
-                        <a href="{{ route('thongke.tangluong') }}">
-                            <div class="container-icon-thongke">
-                                <span class="icon-thongke">
-                                    <img src="{{ asset('icon/icon-thongke-1.png') }}" alt="">
-                                </span>
-                                <h4>Thống kê tăng lương</h4>
-                            </div>
-                        </a>
                     </div>
                     <div class="col right">
                         <a href="{{ route('thongke.hopdong') }}">
@@ -76,19 +56,6 @@
                             </div>
                         </a>
                     </div>
-                    {{-- <div class="col-4 mid">
-                        <form action="{{ route('thongke.hopdong') }}" method="post">
->>>>>>> 069a48bd0c239553ca5d39d248fba1b6823afd59
-                            <input type="text" name="nam" value="{{ $year_now }}" hidden>
-                            @csrf
-                            <button type="submit" class="container-icon-thongke">
-                                <span class="icon-thongke-5">
-                                    <img src="{{ asset('icon/icon-thongke-1.png') }}" alt="">
-                                </span>
-                                <h4>Thống kê hợp đồng</h4>
-                            </button>
-                        </form>
-                    </div> --}}
                 </div>
             </div>
         </div>
